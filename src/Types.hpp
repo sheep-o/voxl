b365ed132f1e713eeedcc67345191eaeaac69be7
Rami Kaldany <ramiik@umich.edu>
2026-08-10 08:29:55 -0400

    push types header

---
+++ src/Types.hpp
@@ -0,0 +1,29 @@
+#ifndef VOXL_TYPES_HPP_
+#define VOXL_TYPES_HPP_
+
+#include <unordered_map>
+#include <queue>
+#include <glm/glm.hpp>
+#include <memory>
+#include "Chunk.hpp"
+
+class Chunk;
+
+struct ivec3Hash {
+    std::size_t operator()(const glm::ivec3 &c) const {
+        std::size_t h1 = std::hash<int>{}(c.x);
+        std::size_t h2 = std::hash<int>{}(c.y);
+        std::size_t h3 = std::hash<int>{}(c.z);
+
+        return h1 ^ (h2 << 1) ^ (h3 << 2);
+    }
+};
+
+using ChunkMap 
+    = std::unordered_map<glm::ivec3, std::shared_ptr<Chunk>, ivec3Hash>;
+using ChunkQueue
+    = std::queue<std::shared_ptr<Chunk>>;
+
+
+
+#endif
